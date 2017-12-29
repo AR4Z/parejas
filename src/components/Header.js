f5fbd45e97adfb5f48d7d8770e28de212b03cff2
Orlando Hernandez <oaraz02@gmail.com>
2017-12-29 14:28:08 -0500

    add material-ui

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+const Header = (props) => {
+  return (
+
+
+  );
+
+}
+
+
+export default connect()(Header);
